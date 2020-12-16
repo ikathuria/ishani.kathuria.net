aa5ae831c92b1ba655a0dde4a7b28aa66beac693
Ishani Kathuria <ishani@kathuria.net>
2020-12-16 15:58:36 +0530

    update

---
+++ data/projects-data.js
@@ -0,0 +1,99 @@
+export default [
+  {
+    slug: "python-games",
+    img: "/projects/python-games/cover.gif",
+    title: "python games",
+    date: "November 2020",
+    excerpt: "Some popular offline games given an online twist with python.",
+    tags: ["Programming", "Python", "Gui"],
+    code: "https://github.com/ikathuria/PythonGames",
+    view: "https://repl.it/@ikathuria/PythonGames",
+    images: [
+      "/projects/python-games/pgCLI.jpg",
+      "/projects/python-games/pgGui1.png",
+      "/projects/python-games/pgGui2.png",
+      "/projects/python-games/pgGui3.png",
+      "/projects/python-games/pgGui4.png",
+      "/projects/python-games/pgGui5.png",
+    ],
+    description:
+      "Some popular offline games given an online twist with python. The games are made in 2 formats, the normal command line text based format and a graphical user interface format made with the help of PyQt5.",
+  },
+
+  {
+    slug: "django-portfolio",
+    img: "/projects/django-portfolio/cover.gif",
+    title: "django portfolio",
+    date: "November 2020",
+    excerpt:
+      "A portfolio web application made with django and deployed on heroku.",
+    tags: ["Programming", "Python", "Django", "Portfolio"],
+    code: "https://github.com/ikathuria/DjangoPortfolio",
+    view: "https://fierce-thicket-91337.herokuapp.com/",
+    images: [
+      "/projects/pg/pgCLI.jpg",
+      "/projects/pg/pgGui1.png",
+      "/projects/pg/pgGui2.png",
+      "/projects/pg/pgGui3.png",
+      "/projects/pg/pgGui4.png",
+      "/projects/pg/pgGui5.png",
+    ],
+    description:
+      "A portfolio web application made with django and deployed on heroku.",
+  },
+
+  {
+    slug: "voice-assistant",
+    img: "/projects/voice-assistant/cover.gif",
+    title: "voice assistant",
+    date: "October 2020",
+    excerpt: "Voice Assistant made using Python.",
+    tags: ["Programming", "Python", "Voice Assistant"],
+    code: "https://github.com/ikathuria/Riley",
+    view: "",
+    images: [],
+    description: "Voice Assistant made using Python.",
+  },
+
+  {
+    slug: "portfolio-website",
+    img: "/projects/portfolio-website/cover.gif",
+    title: "portfolio website",
+    date: "August 2020",
+    excerpt:
+      "A simple portfolio website made with HTML, CSS, Bootstrap and Javascript. Deployed using Github Pages.",
+    tags: ["HTML", "CSS", "Bootstrap", "JavaScript", "Portfolio"],
+    code: "https://github.com/ikathuria/PortfolioWebsite",
+    view: "https://ikathuria.github.io/PortfolioWebsite/",
+    images: [
+      "/projects/portfolio-website/1.png",
+      "/projects/portfolio-website/2.png",
+      "/projects/portfolio-website/3.png",
+      "/projects/portfolio-website/4.png",
+      "/projects/portfolio-website/5.png",
+    ],
+    description:
+      "A simple portfolio website made with HTML, CSS, Bootstrap and Javascript. Deployed using Github Pages.",
+  },
+
+  {
+    slug: "first-website",
+    img: "/projects/first-website/cover.gif",
+    title: "first website",
+    date: "July 2020",
+    excerpt:
+      "A project made while learning CSS with the help of the CSS Essential Training course on LinkedIn.",
+    tags: ["HTML", "CSS", "Portfolio"],
+    code: "https://github.com/ikathuria/FirstWebsite",
+    view: "https://ikathuria.github.io/FirstWebsite/",
+    images: [
+      "/projects/first-website/1.png",
+      "/projects/first-website/2.png",
+      "/projects/first-website/3.png",
+      "/projects/first-website/4.png",
+      "/projects/first-website/5.png",
+    ],
+    description:
+      "A project made while learning CSS with the help of the CSS Essential Training course on LinkedIn.",
+  },
+];
